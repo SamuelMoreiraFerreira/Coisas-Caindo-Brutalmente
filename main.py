233d43eaecd97d73c1b06701518c591c2ed6c3f6
Samuel Moreira Ferreira <samuel.mferreira205@gmail.com>
2024-05-14 14:18:48 -0300

    Add: Background

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ import pygame
 pygame.init()
 pygame.display.set_caption("Coisas Caindo Brutalmente")
 
-screen = pygame.display.set_mode((1080, 1920))
+screen = pygame.display.set_mode((800, 450))
 clock = pygame.time.Clock()
+
+background = pygame.image.load("images/cozinha.jpg")
+background = pygame.transform.scale(background, (800, 450))
 
 running = True
 
 while running:
+
+    screen.blit(background, (0, 0))
 
     for event in pygame.event.get():
 
